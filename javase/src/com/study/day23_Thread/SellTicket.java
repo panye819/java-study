a485f9894e040c4ad4aa3f2ee44383b031b517d5
panye819 <panye819@gmail.com>
2016-11-26 21:56:44 +0800

    1026

--- javase/src/com/study/day23_Thread/SellTicket.java
+++ javase/src/com/study/day23_Thread/SellTicket.java
@@ -5,28 +5,25 @@ package com.study.day23_Thread;
  *
  */
 public class SellTicket implements Runnable{
-	//为了让对象共享这一百张票，应该把
-	private int ticket = 100;
+	private int tickets = 100;
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
-		
 		while(true){
-			synchronized("锁"){			
-				if(ticket>0){
+			//多个线程一定要是同一把锁。
+			synchronized("锁"){
+			if(tickets > 0){
 				try {
 					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				System.out.println(Thread.currentThread().getName()+"正在出售第"+(ticket--)+"张票");
-//				ticket--;
-
 				}
-			}}
-
-		}
+				System.out.println(Thread.currentThread().getName()+"正在出售第:"+(tickets--)+"张票");
+			}
+			}
+		}	
 	}
+	
 
 }
