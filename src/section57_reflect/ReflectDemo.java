f91a42982f104e7d45fe5a7e5d8f91cac519dc62
panye819 <panye819@gmail.com>
2017-12-18 10:43:35 +0800

    add reflect

---
+++ src/section57_reflect/ReflectDemo.java
@@ -0,0 +1,15 @@
+package section57_reflect;
+
+public class ReflectDemo {
+	static {
+		b = 6;
+		System.out.println("------------");
+	}
+	static int a = 5;
+	static int b = 9;
+	static int c;
+	public static void main(String[] args) {
+		System.out.println(ReflectDemo.b);
+	}
+
+}
