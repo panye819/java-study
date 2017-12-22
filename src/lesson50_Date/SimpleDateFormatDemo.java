4fac29e9db307da65b66b3542920eb1b72611af8
panye819 <panye819@gmail.com>
2017-12-22 15:38:07 +0800

    modify date

--- src/lesson50_Date/SimpleDateFormatDemo.java
+++ src/lesson50_Date/SimpleDateFormatDemo.java
@@ -4,27 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Date	 --	 String(格式化)
- * 		public final String format(Date date)
- * 
- * String -- Date(解析)
- * 		public Date parse(String source)
- * 
- * DateForamt:可以进行日期和字符串的格式化和解析，但是由于是抽象类，所以使用具体子类SimpleDateFormat。
- * 
- * SimpleDateFormat的构造方法：
- * 		SimpleDateFormat():默认模式
- * 		SimpleDateFormat(String pattern):给定的模式
- * 			这个模式字符串该如何写呢?
- * 			通过查看API，我们就找到了对应的模式
- * 			年 y
- * 			月 M	
- * 			日 d
- * 			时 H
- * 			分 m
- * 			秒 s
- */
 public class SimpleDateFormatDemo {
 
 	public static void main(String[] args) throws ParseException {
